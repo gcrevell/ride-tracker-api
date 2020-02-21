a98393b422a67042568f700772576cfb06420c5c
Gabriel Revells <gcrevell@mtu.edu>
2020-02-21 09:56:00 -0500

    Basics? Not sure

---
+++ rides.php
@@ -0,0 +1,41 @@
+<?php
+
+// Create connection
+$con=mysqli_connect("localhost","revoltap_RTRead","(/7oDyZG(YvtE;Q{jiFPAcLjGKs4ike92C@Pu(,evMEHHAg4FL3EV3L63}Um]j4g","revoltap_RideTracker");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$parkId = $_GET["parkId"];
+
+// This SQL statement selects ALL from the table 'Locations'
+$sql = "SELECT * FROM Ride WHERE parkId = " . $parkId . " ORDER BY id LIMIT 1";
+
+echo $sql;
+
+// Check if there are results
+if ($result = mysqli_query($con, $sql))
+{
+    // If so, then create a results array and a temporary one
+    // to hold the data
+    $resultArray = array();
+    $tempArray = array();
+
+    // Loop through each row in the result set
+    while($row = $result->fetch_object())
+    {
+        // Add each row into our results array
+        $tempArray = $row;
+        array_push($resultArray, $tempArray);
+    }
+
+    // Finally, encode the array to JSON and output the results
+    echo json_encode($resultArray);
+}
+
+// Close connections
+mysqli_close($con);
+?>
